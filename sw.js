9b8c47bbd2d3072f8975ffe9ee23c5866a05f725
Foy <s225002731@gmail.com>
2026-01-03 11:56:34 +0800

    Updates

--- sw.js
+++ sw.js
@@ -15,10 +15,19 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
+        // Cache hit - return response
         if (response) {
           return response
         }
-        return fetch(event.request)
+        return fetch(event.request).then(
+          (response) => {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response
+            }
+            return response
+          }
+        )
       })
   )
 })
